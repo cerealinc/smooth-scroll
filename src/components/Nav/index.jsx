64fda96b9d6a7963a0d0599da7f5164f281678ec
beamtlc <89999597+beamtlc@users.noreply.github.com>
2023-11-03 13:10:43 -0500

    ST

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,17 +1,30 @@
 
 import React from 'react';
 import Link from 'next/link';
+import gsap from 'gsap';
 import AnimatedLogo from '@/components/AnimatedSection';
 
 import styles from './style.module.css';
 
 const Nav = () => {
+
+      const manageMouseEnter = (e) => {
+        gsap.to(e.target.children, {width: "100%", duration: 0.3})
+      }
+    
+      const manageMouseLeave = (e) => {
+        gsap.to(e.target.children, {width: "0", duration: 0.3, delay: 0.1})
+      }
+    
+
+
+
   return (
     <div className={styles.navigation}>
 
-<Link href="">Contact</Link>
+<Link onMouseEnter={(e) => {manageMouseEnter(e)}} onMouseLeave={(e) => {manageMouseLeave(e)}} href="">Contact<div className={styles.underline}></div></Link>
 <span className={styles.centerLine}></span>
-<Link href="">Home</Link>
+<Link onMouseEnter={(e) => {manageMouseEnter(e)}} onMouseLeave={(e) => {manageMouseLeave(e)}} href="">Home<div className={styles.underline}></div></Link>
     </div>
   );
 };
--- src/components/Scroll/index.jsx
+++ src/components/Scroll/index.jsx
@@ -1,17 +1,64 @@
 
-import React from 'react';
+import React, { useState, useEffect, useLayoutEffect, useRef } from 'react'
 import Link from 'next/link';
 import AnimatedLogo from '@/components/AnimatedSection';
 
 import styles from './style.module.css';
 
 const Scroll = () => {
+    const [scrollPercentage, setScrollPercentage] = useState(0);
+
+    useEffect(()=>{
+
+        const handleScroll = ()=>{
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.documentElement.scrollHeight;
+            const scrollY = window.scrollY;
+            
+            const scrollPercent = (scrollY / (documentHeight - windowHeight))*100 
+    
+            setScrollPercentage(scrollPercent)
+            console.log(scrollPercent)
+        }
+    
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        return()=>{
+            window.removeEventListener("scroll", handleScroll)
+        }
+    
+      },[])
   return (
     <div className={styles.scroll}>
+    <div>scroll</div>
 
-<div>scroll</div>
-<span className={styles.centerLine}></span>
+<div
+      id="progress-container"
+      style={{
+        height: "2px",
+        width: "100%",
+        backgroundColor: "none",
+        position: "relative",
+        marginTop: "8px",
+        left: "0",
+        right: "0",
+      }}
+    >
+      <div
+        className="progress-fill"
+        style={{
+          height: "100%",
+          backgroundColor: "white",
+          width: `${scrollPercentage}%`,
+        }}
+      >
+       
+      </div>
     </div>
+
+    </div>
+
   );
 };
 
