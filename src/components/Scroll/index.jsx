540439cf6daea427c755c69c866cdc986640e6e4
beamtlc <89999597+beamtlc@users.noreply.github.com>
2023-11-05 16:15:48 -0600

    qwd

--- src/components/Scroll/index.jsx
+++ src/components/Scroll/index.jsx
@@ -1,63 +1,54 @@
-
-import React, { useState, useEffect, useLayoutEffect, useRef } from 'react'
-import Link from 'next/link';
-import AnimatedLogo from '@/components/AnimatedSection';
+import React, { useState, useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 import styles from './style.module.css';
 
 const Scroll = () => {
-    const [scrollPercentage, setScrollPercentage] = useState(0);
+  const lineRef = useRef(null);
 
-    useEffect(()=>{
+  useEffect(() => {
+    const line = lineRef.current;
 
-        const handleScroll = ()=>{
-            const windowHeight = window.innerHeight;
-            const documentHeight = document.documentElement.scrollHeight;
-            const scrollY = window.scrollY;
-            
-            const scrollPercent = (scrollY / (documentHeight - windowHeight))*100 
-    
-            setScrollPercentage(scrollPercent)
-        }
-    
-    
-        window.addEventListener("scroll", handleScroll);
-    
-        return()=>{
-            window.removeEventListener("scroll", handleScroll)
-        }
-    
-      },[])
+    // Create a timeline for the repeating animations
+    const animation = gsap.timeline({
+      repeat: -1, // Infinite repeat
+      yoyo: false, // Reverse the animation back and forth
+    });
+
+    // Define the animations
+    animation.fromTo(
+      line,
+      { y: 0, height: '0' },
+      { y: '6px', height: '60px', duration: 1,  ease: "power2.inOut" }
+    );
+
+    animation.fromTo(
+      line,
+      { y: '6px', height: '60px', delay: 2 },
+      { y: '72px', height: '0', duration: 1,  ease: "power2.inOut" }
+    );
+
+    return () => {
+      animation.kill(); // Clean up animation on unmount
+    };
+  }, []);
+
   return (
     <div className={styles.scroll}>
-    <div>Scroll</div>
+      <div className={styles.scrollHeader}>Scroll</div>
 
-<div
-      id="progress-container"
-      style={{
-        height: "2px",
-        width: "100%",
-        backgroundColor: "none",
-        position: "relative",
-        marginTop: "8px",
-        left: "0",
-        right: "0",
-      }}
-    >
-      <div
-        className="progress-fill"
-        style={{
-          height: "100%",
-          backgroundColor: "white",
-          width: `${scrollPercentage}%`,
-        }}
-      >
-       
-      </div>
+        <div
+          ref={lineRef}
+          style={{
+            height: '60px',
+            backgroundColor: 'white',
+            width: '2px',
+            position: "absolute",
+            top: '-24px',
+
+          }}
+        ></div>
     </div>
-
-    </div>
-
   );
 };
 
