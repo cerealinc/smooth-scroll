0a51e06a8bbb64d7b4e850bb419677247eefa838
beamtlc <89999597+beamtlc@users.noreply.github.com>
2023-11-06 09:52:55 -0600

    qwd

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,80 +1,79 @@
 import React, { useEffect, useState } from 'react';
 
 const CursorCircle = () => {
-
-
-    const [scrollPercentage, setScrollPercentage] = useState(0);
-
-    useEffect(() => {
-      const handleScroll = () => {
-        const windowHeight = window.innerHeight;
-        const documentHeight = document.documentElement.scrollHeight;
-        const scrollY = window.scrollY;
-  
-        const scrollPercent = (scrollY / (documentHeight - windowHeight)) * 100;
-  
-        setScrollPercentage(scrollPercent);
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-
-    const calculatedPercentage = Math.min(scrollPercentage, 100); // Ensure it doesn't go over 100%
-    const dashOffset = 283 - (283 * calculatedPercentage) / 100;
-
-
-  // State to store the cursor's position
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-
-  // Update the cursor's position when the mouse moves
-  const handleMouseMove = (e) => {
-    setCursorPosition({ x: e.clientX, y: e.clientY });
-  };
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   useEffect(() => {
-    // Add a mousemove event listener to track cursor position
-    window.addEventListener('mousemove', handleMouseMove);
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollY = window.scrollY;
 
-    // Clean up the event listener when the component unmounts
+      const scrollPercent = (scrollY / (documentHeight - windowHeight)) * 100;
+
+      setScrollPercentage(scrollPercent);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
-  return (
+  const calculatedPercentage = Math.min(scrollPercentage, 100); // Ensure it doesn't go over 100%
+  
+  // Adjust the strokeDasharray based on the new SVG size
+  const svgSize = 60;
+  const circleRadius = 25;
+  const circumference = 2 * Math.PI * circleRadius;
+  const dashArray = (circumference * calculatedPercentage) / 100;
+  const dashOffset = circumference - dashArray;
 
+  // Rest of your code...
+ // State to store the cursor's position
+ const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+ // Update the cursor's position when the mouse moves
+ const handleMouseMove = (e) => {
+   setCursorPosition({ x: e.clientX -30, y: e.clientY -30 });
+ };
+
+ useEffect(() => {
+   // Add a mousemove event listener to track cursor position
+   window.addEventListener('mousemove', handleMouseMove);
+
+   // Clean up the event listener when the component unmounts
+   return () => {
+     window.removeEventListener('mousemove', handleMouseMove);
+   };
+ }, []);
+  return (
     <svg
-      width="100"
-      height="100"
+      width={svgSize}
+      height={svgSize}
       style={{
         position: 'fixed',
-        pointerEvents: 'none', // Make the SVG ignore mouse events
-        zIndex: 9999, // Ensure it's on top of other elements
-        transform: `translate(${cursorPosition.x}px, ${cursorPosition.y}px)`, // Move the SVG to the cursor's position
+        pointerEvents: 'none',
+        zIndex: 9999,
+        transform: `translate(${cursorPosition.x}px, ${cursorPosition.y}px)`,
       }}
-    >    <circle cx="50" cy="50" r="45" stroke="white" strokeWidth="4" fill="transparent" />
-    <circle
-      cx="50"
-      cy="50"
-      r="45"
-      stroke="#333"
-      strokeWidth="4"
-      fill="transparent"
-      style={{
-        strokeDasharray: 283, // 2 * π * r
-        strokeDashoffset: dashOffset
-      }}
-    />
-  </svg>
-
+    >
+      <circle cx={svgSize / 2} cy={svgSize / 2} r={circleRadius} stroke="white" strokeWidth="2" fill="transparent" />
+      <circle
+        cx={svgSize / 2}
+        cy={svgSize / 2}
+        r={circleRadius}
+        stroke="#333"
+        strokeWidth="2"
+        fill="transparent"
+        style={{
+          strokeDasharray: circumference,
+          strokeDashoffset: dashOffset,
+        }}
+      />
+    </svg>
   );
 };
 
 export default CursorCircle;
-
-
-
