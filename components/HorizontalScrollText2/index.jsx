a925d3f53a8c91132bbe07f300676fa96bbb140e
beamtlc <89999597+beamtlc@users.noreply.github.com>
2023-11-07 16:52:16 -0600

    qwd

--- components/HorizontalScrollText2/index.jsx
+++ components/HorizontalScrollText2/index.jsx
@@ -10,20 +10,10 @@ const HorizontalScrollText2 = () => {
   const textRef = useRef(null);
   const containerRef = useRef(null);
   const videoRef = useRef(null);
-  const headerRef = useRef(null);
-
+  const videoContainerRef = useRef(null);
+  
   useEffect(() => {
-    // Parallax effect for the text
-    gsap.to(headerRef.current, {
-      y: 196, // Adjust the vertical parallax distance
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: 'top-=200 center',
-        end: 'center center',
-        scrub: true, // Smooth scrolling effect
-        markers: false
-      },
-    });
+
 
     // Parallax effect for the video
     gsap.to(videoRef.current, {
@@ -41,25 +31,19 @@ const HorizontalScrollText2 = () => {
     const itemWidth = item.clientWidth;
 
     gsap.set(item, {
-      marginLeft: '100vw',
+      marginLeft: '100vw', // Start off the screen to the right
     });
 
     const marquee = gsap.to(item, {
-      duration: 15,
+      duration: 45,
       ease: 'none',
-      x: '-=' + (itemWidth + window.innerWidth),
-      repeatRefresh: true, // Repeats animation when it reaches the end
-
+      x: '-=' + (itemWidth + window.innerWidth), // Animate off the screen to the left
       repeat: -1,
+      repeatRefresh: true, // Repeats animation when it reaches the end
       modifiers: {
         x: x => (parseFloat(x) % (itemWidth + window.innerWidth)) + 'px',
       },
     });
-
-    const master = gsap.timeline().add(marquee, 0);
-
-    const tween = gsap.to(master, { duration: 1.5, timeScale: 1, paused: true });
-    const timeScaleClamp = gsap.utils.clamp(1, 6);
 
     ScrollTrigger.create({
       trigger: containerRef.current,
@@ -68,10 +52,7 @@ const HorizontalScrollText2 = () => {
       pin: true,
       pinSpacing: true,
       markers: false,
-      onUpdate: self => {
-        master.timeScale(timeScaleClamp(Math.abs(self.getVelocity() / 200)));
-        tween.invalidate().restart();
-      },
+
     });
 
     // Create a ScrollTrigger to control the video blur effect
@@ -93,22 +74,31 @@ const HorizontalScrollText2 = () => {
   }, []);
 
   return (
+    <div className={styles.wrapper}>
+    <div className={styles.childWrapper}>
+    <div ref={textRef} className={styles.scrollText}>
+    Creative Direction, Development, and Execution
+<span style={{marginLeft: "20vw"}}>        Creative Direction, Development, and Execution
+
+</span><span style={{marginLeft: "20vw"}}>        Creative Direction, Development, and Execution
+
+</span><span style={{marginLeft: "20vw"}}>        Creative Direction, Development, and Execution
+
+</span>
+    </div>
+    </div>
+
     <div ref={containerRef} className={styles.scrollContainer}>
-      <div ref={headerRef} className={styles.scrollHeader}>
-        A right hand to strategy
-      </div>
-      <div ref={textRef} className={styles.scrollText}>
-        A creative partner for agencies, brands, and start-ups.
-      </div>
-      <div className={styles.scrollVideo}>
+
       <div ref={videoRef} className={styles.scrollVideoBlur}>
 
         <video loop muted autoPlay playsInline className="videoInner">
           <source src="/images/ST_2020_ActiveReel_5.mp4" type="video/mp4" />
         </video>
         </div>
-      </div>
     </div>
+    </div>
+
   );
 };
 
