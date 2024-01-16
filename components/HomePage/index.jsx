2d62e8e468e44776f2c6e9f5b22bd307779e8376
beamtlc <89999597+beamtlc@users.noreply.github.com>
2024-01-16 16:49:33 -0600

    qwdqd

--- components/HomePage/index.jsx
+++ components/HomePage/index.jsx
@@ -11,35 +11,57 @@ const projects = [
     id: "one",
     title: "Miramax",
     img: "2.jpg",
-    details: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><span>Creative Direction</span> <span>Development</span> <span>Production</span>",
+    details: "Lorem ipsum dolor sit amet ut labore et dolore magna aliqua. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    tags: [
+      "Creative Direction",
+      "Production"
+    ],
     src: "saint-steven-taylor-halston-netflix.mp4"
   },
   {
     id: "two",
     title: "New Era",
     img: "2.jpg",
-    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.<span>Creative Direction</span> <span>Development</span> <span>Production</span>",
+    details: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    tags: [
+      "Development",
+      "Production"
+    ],
     src: "Krewe2020_Thumbnail.mp4"
   },
   {
     id: "three",
     title: "Walmart",
     img: "2.jpg",
-    details: "<p>Project Info, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p><span>Creative Direction</span> <span>Development</span> <span>Production</span>",
+    details: "Project Info, Enim ad minim veniam lorem ipsum dolor sit amet consectetur adipiscing elit.",
+    tags: [
+      "Creative Direction",
+      "Development",
+      "Production"
+    ],
     src: "saint-studio-GME_SRT.mp4"
   },
   {
     id: "four",
     title: "Miramax",
     img: "2.jpg",
-    details: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><span>Creative Direction</span> <span>Development</span> <span>Production</span>",
+    details: "Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliequat.",
+    tags: [
+      "Development",
+      "Production"
+    ],
     src: "ST_2020_ActiveReel_5.mp4"
   },
   {
     id: "five",
     title: "New Era",
     img: "2.jpg",
-    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam<span>Creative Direction</span> <span>Development</span> <span>Production</span>",
+    details: "Lorem ipsum dolor sit amet, consectetur ad quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    tags: [
+      "Creative Direction",
+      "Development",
+      "Production"
+    ],
     src: "saint-steven-taylor-Official_MP_Trailer_Final.mp4"
   }
 ]
@@ -47,20 +69,16 @@ const HomePage = () => {
   const swapText = useRef(null);
   const textRef = useRef(null);
   const textRef2 = useRef(null);
-
   const textRef2Wrap = useRef(null);
-
-  
-  const textRef4Wrap = useRef(null);
   const textRef4 = useRef(null);
   const textRef5 = useRef(null);
   const containerRef = useRef(null);
   const videoRef = useRef(null);
   const wrapperRef = useRef(null);
-  const projectWrapperRef = useRef(null);
   const projectWrapperRefOuter = useRef(null);
   const featuredReel = useRef(null);
   const gridContainer = useRef(null);
+  const projectVideoRef = useRef(null); // Move this line to the top
 
 
   const [firstHover, setFirstHover] = useState(false);
@@ -75,29 +93,30 @@ const HomePage = () => {
 
 
 
-      const gridItems = document.querySelectorAll('.gridItem');
+    const gridItems = document.querySelectorAll('.gridItem');
 
-      gsap.to(gridItems, {
-        scrollTrigger: {
-          trigger: gridContainer.current,
-          start: 'top 80%', // Adjust the start position as needed
-        },
-        opacity: 1,
-        marginTop: 0,
-        stagger: 0.2, // Adjust the stagger value as needed
-        duration: 0.8,
-        ease: 'power2.inOut',
-      });
- 
+    gsap.to(gridItems, {
+      scrollTrigger: {
+        trigger: gridContainer.current,
+        start: 'top 80%', // Adjust the start position as needed
+      },
+      opacity: 1,
+      marginTop: 0,
+      stagger: 0.2, // Adjust the stagger value as needed
+      duration: 0.8,
+      ease: 'power2.inOut',
+    });
+
 
 
 
     const worksArray = Array.from(document.querySelectorAll('.flexItemWorks'));
     setWorks(worksArray);
-    
+
     worksArray.forEach((work) => {
-      const image = work.querySelector('img');
-    
+      const video = work.querySelector('video');
+      const videoWrap = work.querySelector('.projectVideo');
+      
       // First gsap.to code for scaling in
       gsap.to(work, {
         scrollTrigger: {
@@ -109,39 +128,40 @@ const HomePage = () => {
           onUpdate: ({ progress, direction, isActive }) => {
             const scaleValue = 1 - progress * 0.1;
             const scaleValue3 = progress * 12;
-            const scaleValue2 = progress * 122;
-            gsap.set(work, {
+            const scaleValue2 = progress * 40;
+            
+            gsap.set(videoWrap, {
               scale: scaleValue,
-              boxShadow: `${scaleValue3}px ${scaleValue3}px 12px rgba(0, 0, 0, 0.1)`,
               marginBottom: `${scaleValue2}px`
             });
           },
         },
       });
-    
+
       // Second gsap.to code for scaling out
       gsap.to(work, {
         scrollTrigger: {
           trigger: work,
           start: 'top bottom', // Adjust the start value for scaling out
-          end: 'top-=140 top',
+          end: 'top center',
           scrub: true,
           markers: false,
-          onUpdate: ({ progress, direction, isActive }) => {
+          onUpdate: ({ progress, direction, isActive, self }) => {
             const scaleValue = 0.9 + progress * 0.1; // Adjust the scaling factor for scaling out
             const scaleValue3 = 2 - progress * 12; // Adjust the scaling factor for scaling out
-            const scaleValue2 =  progress / 20;
+            const scaleValue2 = progress / 40;
+            const blurAmount = 8 - progress * 8;
+            gsap.set(video, { filter: `blur(${blurAmount}px)` });
 
-            gsap.set(work, {
+            gsap.set(videoWrap, {
               scale: scaleValue,
-              boxShadow: `-${scaleValue3}px -${scaleValue3}px 6px rgba(0, 0, 0, 0.3)`,
               marginTop: `${scaleValue2}px`
             });
           },
         },
       });
     });
-    
+
 
 
     // Create a ScrollTrigger to control the video blur effect
@@ -168,17 +188,9 @@ const HomePage = () => {
       setWorkInView(true);
       gsap.to(videoRef.current, { opacity: 0, delay: .1, duration: 1 });
       gsap.to(textRef.current, { opacity: 0, marginTop: '-80px', duration: 2 });
-      gsap.to(textRef2.current, { opacity: 1, y: 0, duration: 3 });
       gsap.to(textRef5.current, { opacity: 0.5, marginTop: '-80px', duration: 2 });
       gsap.to(textRef4.current, { opacity: 1, marginTop: '-80px', duration: 2 });
       gsap.to(focusFrontRef.current, { opacity: 1, duration: 2 });
-      gsap.to(gridItems, {
-        opacity: 0,
-        y: '-20px',
-        stagger: 0.2, // Adjust the stagger value as needed
-        duration: 0.8,
-        ease: 'power2.inOut',
-      });
 
     };
 
@@ -188,72 +200,54 @@ const HomePage = () => {
       gsap.to(focusFrontRef.current, { opacity: 0, duration: 2 });
       gsap.to(videoRef.current, { opacity: 1, delay: 1, duration: 1 });
       gsap.to(textRef.current, { opacity: 1, marginTop: 0, duration: 1 });
-      gsap.to(textRef2.current, { opacity: .5, y: 100, duration: 3 });
       gsap.to(textRef5.current, { opacity: 1, marginTop: 0, duration: 2 });
       gsap.to(textRef4.current, { opacity: 0, marginTop: 0, duration: 2 });
-      gsap.to(gridItems, {
-        opacity: 1,
-        y: '0px',
-        stagger: 0.2, // Adjust the stagger value as needed
-        duration: 0.8,
-        ease: 'power2.inOut',
-      });
     };
 
 
-      // Create ScrollTrigger with onEnter and onLeaveBack functions for the outer wrapper
-ScrollTrigger.create({
-  trigger: projectWrapperRefOuter.current,
-  start: 'top center-=200',
-  end: 'top center-=200',
-  pin: false, // Pin the outer wrapper
-  pinSpacing: false, // Adjust pinSpacing based on your layout needs
-  markers: false,
-  onEnter: onEnterFunction,
-  onLeaveBack: onLeaveBackFunction,
-});
+    // Create ScrollTrigger with onEnter and onLeaveBack functions for the outer wrapper
+    ScrollTrigger.create({
+      trigger: projectWrapperRefOuter.current,
+      start: 'top center-=200',
+      pin: false, // Pin the outer wrapper
+      pinSpacing: false, // Adjust pinSpacing based on your layout needs
+      markers: false,
+      onEnter: onEnterFunction,
+      onLeaveBack: onLeaveBackFunction,
+    });
 
-// Create ScrollTrigger for the scrolling content inside the outer wrapper
-ScrollTrigger.create({
-  trigger: projectWrapperRef.current,
-  start: 'top top+=140',
-  end: 'bottom bottom',
-  scrub: true, // Allow scrolling inside the pinned outer wrapper
-  markers: false,
-  onEnter: onEnterFunction,
-  onLeaveBack: onLeaveBackFunction,
-});
+
 
   }, []);
-// Add this code within your useEffect to handle scroll and clip the text
-useEffect(() => {
-  const handleScroll = () => {
-    const scrollClipRef = textRef2Wrap.current;
-    const scrollTextRef = textRef2.current;
+  // Add this code within your useEffect to handle scroll and clip the text
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollClipRef = textRef2Wrap.current;
+      const scrollTextRef = textRef2.current;
 
-    if (scrollClipRef && scrollTextRef) {
-      const scrollClipRect = scrollClipRef.getBoundingClientRect();
-      const scrollTextRect = scrollTextRef.getBoundingClientRect();
+      if (scrollClipRef && scrollTextRef) {
+        const scrollClipRect = scrollClipRef.getBoundingClientRect();
+        const scrollTextRect = scrollTextRef.getBoundingClientRect();
 
-      // Check if scrollText is behind scrollClip
-      if (scrollTextRect.bottom < scrollClipRect.top) {
-        // Apply clip to hide scrollText when it's behind scrollClip
-        scrollTextRef.style.clipPath = 'inset(100% 100% 100% 100%)';
-      } else {
-        // Reset clip if scrollText is visible
-        scrollTextRef.style.clipPath = 'none';
+        // Check if scrollText is behind scrollClip
+        if (scrollTextRect.bottom < scrollClipRect.top) {
+          // Apply clip to hide scrollText when it's behind scrollClip
+          scrollTextRef.style.clipPath = 'inset(100% 100% 100% 100%)';
+        } else {
+          // Reset clip if scrollText is visible
+          scrollTextRef.style.clipPath = 'none';
+        }
       }
-    }
-  };
+    };
 
-  // Add event listener for scroll
-  window.addEventListener('scroll', handleScroll);
+    // Add event listener for scroll
+    window.addEventListener('scroll', handleScroll);
 
-  // Clean up the event listener on component unmount
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
 
   const handleProjectHover = () => {
@@ -297,9 +291,9 @@ useEffect(() => {
       </div>
 
       <div ref={containerRef} className={styles.scrollContainer}>
-<div className={styles.featuredReel} ref={featuredReel}>
-Featured Commissions
-</div>
+        <div className={styles.featuredReel} ref={featuredReel}>
+          Featured Commissions
+        </div>
         <div ref={videoRef} className={styles.scrollVideoBlur}>
 
           <video loop muted autoPlay playsInline className="videoInner">
@@ -311,68 +305,18 @@ Featured Commissions
 
 
 
-      <div  ref={projectWrapperRefOuter} className={styles.projectWrapperOuter} id='your-anchor-2'>
-
-        <div ref={projectWrapperRef} className={styles.projectWrapper} id='your-anchor-id'>
-
-          <div className={styles.overlay}></div>
+      <div ref={projectWrapperRefOuter} className={styles.projectWrapperOuter} id='your-anchor-2'>
 
 
-          <div className={styles.projectDetails}>
-            {
-              projects.map(({ id, details }) => (
-                // eslint-disable-next-line react/jsx-key
-                <span
-                  key={id}
-                  style={{
-                    display:
-                      activeId === id ? "block" : "none"
-                  }}
 
-                  dangerouslySetInnerHTML={{ __html: details }}>
-                </span>
 
-              )
-              )
-            }
+        <div ref={textRef2} className={styles.scrollText2} style={{ display: '' }}>
+          {
+            projects.map(({ id, title, details, img, src, tags }) => (
+              // eslint-disable-next-line react/jsx-key
+              <div ref={works} className={`${styles.flexItem} flexItemWorks`}>
+                  <div className={`${styles.projectVideo} projectVideo`}>
 
-          </div>
-          <div ref={focusFrontRef} className={`${styles.focusFront} ${focusClasses}`}>
-
-            <div className={styles.overlay}></div>
-            {
-              projects.map(({ id, src }) => (
-                <video
-                  loop
-                  muted
-                  autoPlay
-                  playsInline
-                  key={id}
-                  className={styles.video}
-                  style={{
-                    display:
-                      activeId === id ? "none" : "none"
-                  }}
-                >
-                  <source src={`/images/${src}`} type="video/mp4" />
-
-                </video>
-              )
-              )
-            }
-
-          </div>
-
-        </div>
-      </div>
-
-      <div className={styles.parent}>
-
-      <div ref={textRef2} className={styles.scrollText2} style={{ display: '' }}>
-        {
-          projects.map(({ id, title, details, img, src }) => (
-            // eslint-disable-next-line react/jsx-key
-            <div ref={works} className={`${styles.flexItem} flexItemWorks`}>
                 <video
                   loop
                   muted
@@ -387,21 +331,44 @@ Featured Commissions
                   <source src={`/images/${src}`} type="video/mp4" />
 
                 </video>
-              <span
-                ref={swapText}
-                key={id}
-                className={styles.listItem}
-                onMouseEnter={() => [setActiveElementOnHover(id), handleProjectHover()]}
-              >
-              </span>
-            </div>
+                </div>
 
-          ))
-        }
+                <div className={styles.projectDetails}>
+                <div className={styles.projectDetailsInner}>
+
+                <div
+                  ref={swapText}
+                  key={id}
+                  className={styles.projectHeader}
+                >
+                  {title}
+                </div>
+                <p
+                                key={id}
+                                style={{
+                                  display:
+                                    activeId === id ? "block" : "block"
+                                }}
+              
+                                dangerouslySetInnerHTML={{ __html: details }}>
+                              </p>
+                              <div className={styles.projectTags} key={id}>
+                              {tags.map((tag, index) => (
+                  <React.Fragment key={index}>
+                    {index > 0 && <span className={styles.bullet}> • </span>}
+                    {tag}
+                  </React.Fragment>
+                ))}                              </div>
+              </div>
+              </div>
+              </div>
+
+            ))
+          }
 
 
+        </div>
       </div>
-    </div>
     </div>
 
   );
