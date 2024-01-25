2031fd051f68071ab582a1c3d316406fa8cf4bf5
beamtlc <89999597+beamtlc@users.noreply.github.com>
2024-01-25 15:41:19 -0600

    qwd

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   
   
     const lenisOptions = isMobile() 
-      ? { lerp: 1, smoothTouch: false } // Adjust these values for mobile
+      ? { lerp: 1, duration: 1.5, smoothTouch: true } // Adjust these values for mobile
       : { lerp: 0.1, duration: 1.5, smoothTouch: true }; // Default values for non-mobile
   
   const fadeClass = renderMain ? (fadeOut ? 'fadeOut' : 'fadeIn') : '';
